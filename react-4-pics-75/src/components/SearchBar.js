75168769dc977d2afcf7fb5df8af26444d7e0f05
Carlos Cabrera Gentile <carloscgentile@gmail.com>
2020-02-19 17:04:03 -0300

    added state to SearchBar component

--- react-4-pics-75/src/components/SearchBar.js
+++ react-4-pics-75/src/components/SearchBar.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = {
+        term: ''
+    };
+
     // option 1, create this function linked to input
     onInputChange(event) {
         console.log(event.target.value);
@@ -16,7 +20,10 @@ class SearchBar extends React.Component {
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange} />
+                        <input 
+                        type="text" 
+                        value={this.state.term}
+                        onChange={ e=> this.setState({term: e.target.value}) } />
                     </div>
                 </form>
             </div>
