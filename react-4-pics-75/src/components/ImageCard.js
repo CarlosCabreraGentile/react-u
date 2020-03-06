f02c896d46aa3ab60c3546457ab8534a2f37f654
Carlos Cabrera Gentile <carloscgentile@gmail.com>
2020-03-06 17:22:01 -0300

    display images on screen

--- react-4-pics-75/src/components/ImageCard.js
+++ react-4-pics-75/src/components/ImageCard.js
@@ -1,14 +1,38 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            span: 0
+        };
+
+        // Create a React Ref
+        this.imageRef = React.createRef();
+    }
+    
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10);
+
+        this.setState({ spans: spans });
+    }
+    
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
     render() {
 
         // Destructuring array, variables come from this.props.image
         const { description, urls } = this.props.image;
 
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+                {/* ref is a JSX tag, not a HTML tag */}
                 <img
+                    ref={this.imageRef}
                     alt={description}
                     src={urls.regular}
                 />
