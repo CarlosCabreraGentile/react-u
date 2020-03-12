d7319b999f151ee4612ab778c94130651c3d61e7
Carlos Cabrera Gentile <carloscgentile@gmail.com>
2020-03-12 10:57:15 -0300

    set up axios config

--- react-5-videos-109/src/apis/youtube.js
+++ react-5-videos-109/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyAG1B1gcskk9DiKNG8gHjuKgdDrAP3sqXE';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
