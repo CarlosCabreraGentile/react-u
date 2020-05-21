a9d39674dcbde18e542ee5923c9cb326a5485bd7
Carlos Cabrera Gentile <carloscgentile@gmail.com>
2020-05-21 16:20:38 -0300

    showing the list of songs

--- react-6-songs-143/src/components/SongList.js
+++ react-6-songs-143/src/components/SongList.js
@@ -6,9 +6,33 @@ import { connect } from 'react-redux';
 
 // option 2
 class SongList extends Component {
+    // take the list of songs and map them and return a JSX
+    renderList() {
+        return this.props.songs.map(
+            song => {
+                return (
+                    <div className="item" key={song.title}>
+                        <div className="right floated content">
+                            <button className="ui button primary">
+                                Select
+                            </button>
+                        </div>
+                        <div className="content">{song.title}</div>
+                    </div>
+                );
+            });
+    }
     render() {
-        return <div>SongList</div>;
+        return <div className="ui divided list">{this.renderList()}</div>;
     }
 }
+
+// this name it´s used by convention, we can call it anything we want
+// takes all the data from the store(get the state) all the data inside a redux store 
+// run some calculation and show up as props inside a component
+const mapStateToProps = (state) => {
+    return { songs: state.songs };
+}
+
 //             call connect() function and then invoke the function
-export default connect()(SongList);
+export default connect(mapStateToProps)(SongList);
