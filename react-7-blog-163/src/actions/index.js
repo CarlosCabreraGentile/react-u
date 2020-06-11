61bb9a57178cdfe445a081b08558facbbfdc7441
Carlos Cabrera Gentile <carloscgentile@gmail.com>
2020-06-11 17:54:31 -0300

    194: MEMOIZATION DONE

--- react-7-blog-163/src/actions/index.js
+++ react-7-blog-163/src/actions/index.js
@@ -1,4 +1,11 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+/**
+ * With MEMOIZE function we can call it with an unique argument one time
+ * after that just return the previous value, without making again the request
+ * save in memory that call and the values returned
+ */
 
 // with API calls (asynchronous call) we have to use middleware to handle this
 // amd to avoid the normal flow of action and reducers get broken
@@ -30,11 +37,16 @@ export const fetchPosts = () => async dispatch => {
 // };
 
 // a function that returns a function
-export const fetchUser = (id) => async dispatch => {
+export const fetchUser = (id) => dispatch => {
+    _fetchUser(id, dispatch);
+}
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    // makes the request and dispatch an action one time
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({
         type: 'FETCH_USER',
         payload: response.data
     });
-}
+});
